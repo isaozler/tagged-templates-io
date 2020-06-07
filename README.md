a8dcc529e76feaca505c0fe134f9ea6ebc4071af
Isa Ozler <contactme@isaozler.com>
2020-06-07 05:16:07 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,114 @@
+# Read me
+
+In most projects we use interpolations for several reasons. This could be simple html class-names, sql queries, or even more complicated strings that needs to be mutated on the fly. This is what this package is good for. You can easily create your own dynamic strings, with conditions, expressions or whatever you need.
+
+## Basic Usage
+
+```
+taggedTemplate(`template-string`, values, conditions?);
+```
+
+### Examples
+
+
+## Basic
+```
+taggedTemplate(`Hello ${0}`, 'world');
+
+// Hello World
+
+taggedTemplate(`Hello {target}`, { target: 'World'});
+
+// Hello World
+
+taggedTemplate(`Hello {target}`, { target: ['World', 'Mars']}, { target: false });
+
+```
+
+## Html class names
+```
+const isFirstNavItem = index === 0;
+
+const className = taggedTemplate(
+  `link__{1}--{0} link__type--{1}`,
+  [['active',''], 'nav'],
+  [isFirstNavItem, true]
+);
+
+<h1 :class="className">Title</h1> // VueJs Sample
+
+// Output
+
+<h1 class="link__nav--active link__type--nav">Title</h1>
+
+```
+
+## SQL
+```
+const isClient = true;
+const asCol = 'name';
+const from = ['clientTable', 'relationTable'];
+const fromCondition = isClient;
+const where = `${as} = ${42}`;
+const start = 0, end = 10;
+
+taggedTemplate(`
+  SELECT {select} AS {as}
+  FROM {from}
+  WHERE {where}
+  LIMIT {start}, {end}
+`
+  ,{ select: 'firstname', as: asCol, from, where, start, end }
+  ,{ from: fromCondition }
+);
+
+
+// Outputs
+
+SELECT firstname AS name
+  FROM clientTable
+  WHERE name = 42
+  LIMIT 0, 10
+
+```
+
+
+## HTML
+
+```
+
+const classHandler = (id, activeIndex, classes) => {
+  if (!!classes) {
+     return taggedTemplate(
+       `class="{0}"`, 
+       [[Array.from(new Set(classes)).join(' '), classes[0]]],
+       [id === activeIndex]
+     );
+  }
+};
+
+const activeIndex = 1;
+
+const result = taggedTemplate(
+  (listWrapper, listItems, activeClass, activeIndex) => `
+    <ul class="${listWrapper}">
+        ${listItems.reduce((ul, {id, label}) => {
+          return ul+`<li id="${id}" ${classHandler(id, activeIndex, activeClass)}>${label}</li>`;
+        }, '')}
+    </ul>
+  `,
+  ['list__wrapper', [{ id: 1, label: 'Item 1' }, { id: 2, label: 'Item 2' }], ['list__item','list__item--active'], activeIndex]
+);
+
+// Outputs
+
+<ul class="list__wrapper">
+  <li id="1" class="list__item list__item--active">
+    Item 1
+  </li>
+  <li id="2" class="list__item">
+    Item 2
+  </li>
+</ul>
+```
+
---
+++ src/index.js
@@ -0,0 +1,175 @@
+'use-strict'
+
+function taggedTemplates(f, v, c) {
+  const clean = (string) => string.toLowerCase().trim();
+
+  const checkBool = (cVal) => clean(`${cVal}`) === 'false' || clean(`${cVal}`) === 'true';
+
+  const process = (fn, data, condition) => {
+    let parseData;
+    if (!!Array.isArray(condition) && typeof fn === 'function') {
+      if (!!Array.isArray(data) && data.length === condition.length) {
+        data = data.reduce((acc, dataItem, index) => {
+          let result = null;
+          if (!!Array.isArray(dataItem) && checkBool(condition[index])) {
+            const [tV, fV] = dataItem;
+            result = JSON.parse(condition[index]) ? tV : fV;
+          } else {
+            result = dataItem;
+          }
+          return [
+            ...acc,
+            result,
+          ];
+        }, []);
+      } else {
+        data = data.reduce((acc, dataItem, index) => {
+          let result = null;
+          if (!!Array.isArray(condition)) {
+            if (!!Array.isArray(dataItem)) {
+              const [tV, fV] = dataItem;
+              result = JSON.parse(condition[index]) ? tV : fV;
+            } else {
+              result = dataItem;
+            }
+          } else if (!Array.isArray(condition) && !!Array.isArray(dataItem)) {
+            const [tV] = dataItem;
+            result = tV;
+          } else if (!Array.isArray(condition) && !Array.isArray(dataItem)) {
+            result = dataItem;
+          }
+          return [
+            ...acc,
+            result,
+          ];
+        }, []);
+      }
+      const da = (sa) => fn.apply(this, sa);
+      parseData = () => da(data);
+    } else if (!!condition && typeof condition !== 'object') {
+      // let result;
+      if (!!Array.isArray(data)) {
+        const [tV, fV] = [...data];
+        if (checkBool(condition)) {
+          data = JSON.parse(condition) ? tV : fV;
+        } else {
+          data = tV;
+        }
+      } else {
+        data = [data];
+      }
+      parseData = () => ((sa) => fn.apply(this, [sa]))(data);
+    } else if (!!Array.isArray(data) && typeof fn === 'function') {
+      // let result;
+      if (!!condition && checkBool(condition)) {
+        const [tV, fV] = [...data];
+        data = JSON.parse(condition) ? [tV] : [fV];
+        parseData = () => ((sa) => fn.apply(this, sa))(data);
+      } else {
+        parseData = () => ((sa) => fn.apply(null, sa))(data);
+      }
+
+    } else if (typeof fn === 'function') {
+      data = [data];
+      parseData = () => ((sa) => {
+        return fn.apply(this, [...sa]);
+      })(data);
+    } else if (!!data && (typeof data === 'string' || typeof data === 'number') && typeof fn === 'string' && !condition) {
+      let callback;
+      if (fn.match(/{(\d+)}/g)) {
+        if (!!Array.isArray(data)) {
+          callback = (_, m) => data[m];
+        } else {
+          callback = () => data;
+        }
+        parseData = () => fn.replace(/{(\d+)}/g, callback);
+      } else if (fn.match(/{(\w+)}/g)) {
+        parseData = (ddd) => {
+          return Object.keys(ddd).reduce((accD, dkk) => {
+            const strRegExPattern = `\\{${dkk}\\}`;
+            const occ = fn.match(new RegExp(strRegExPattern, 'g'));
+            return accD = occ.reduce((acc, matchString) => {
+              const dK = matchString.slice(1, -1);
+              return acc = acc.replace(matchString, ddd[dK]);
+            }, accD);
+          }, fn);
+        };
+      }
+    } else if (!!condition && Array.isArray(condition)) {
+      let callback;
+      if (fn.match(/{(\d+)}/g)) {
+        if (!!Array.isArray(data) && data.length === condition.length) {
+          data = data.reduce((acc_mm, d, i) => {
+            if (!!Array.isArray(d)) {
+              const [tV, fV] = [...d];
+              if (checkBool(condition[i])) {
+                d = JSON.parse(condition[i]) ? tV : fV;
+              } else {
+                d = tV;
+              }
+            } else {
+              d = [d];
+            }
+            return [
+              ...acc_mm,
+              d,
+            ];
+          }, []);
+          callback = (_, m) => data[m];
+        } else {
+          callback = () => data;
+        }
+        parseData = () => fn.replace(/{(\d+)}/g, callback);
+      }
+    } else if (!!data && Array.isArray(data) && typeof fn === 'string' && !condition) {
+      if (fn.match(/{(\d+)}/g)) {
+        if (!!Array.isArray(data)) {
+          callback = (_, m) => data[m];
+        } else {
+          callback = () => data;
+        }
+        parseData = () => fn.replace(/{(\d+)}/g, callback);
+      }
+    } else if (!!data && typeof data === 'object' && !!condition) {
+      if (typeof condition === 'object') {
+        parseData = (ddd) => {
+          return Object.keys(ddd).reduce((accD, dkk) => {
+            let aDk;
+            const strRegExPattern = `\\{${dkk}\\}`;
+            const occ = fn.match(new RegExp(strRegExPattern, 'g'));
+            return accD = occ.reduce((acc, matchString) => {
+              const dK = matchString.slice(1, -1);
+              if (!!ddd[dK] && Array.isArray(ddd[dK]) && checkBool(condition[dK])) {
+                const [tV, fV] = ddd[dK];
+                aDk = JSON.parse(condition[dK]) ? [tV] : [fV];
+              } else {
+                if (!!ddd[dK] && Array.isArray(ddd[dK])) {
+                  const [tV, fV] = ddd[dK];
+                  aDk = !!condition[dK] ? tV : fV;
+                } else if (!!ddd[dK] || !isNaN(ddd[dK])) {
+                  aDk = ddd[dK];
+                }
+              }
+              return acc = acc.replace(matchString, aDk);
+            }, accD);
+          }, fn);
+        };
+      }
+    } else if (!!data && typeof data === 'object' && !condition) {
+      parseData = (ddd) => {
+        return Object.keys(ddd).reduce((accD, dkk) => {
+          const strRegExPattern = `\\{${dkk}\\}`;
+          const occ = fn.match(new RegExp(strRegExPattern, 'g'));
+          return accD = occ.reduce((acc, matchString) => {
+            const dK = matchString.slice(1, -1);
+            return acc = acc.replace(matchString, ddd[dK]);
+          }, accD);
+        }, fn);
+      };
+    }
+    return parseData(data);
+  };
+  return process(f, v, c);
+}
+
+module.exports = taggedTemplates;
